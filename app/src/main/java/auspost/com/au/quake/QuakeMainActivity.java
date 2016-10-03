d8ef9dfabd71833bf57f7b7e40d9959d6371d90f
Angara Raghunandan <angara.raghunandan@pactera.com.au>
2016-10-03 19:24:56 +1100

    Initial commit - Empty Template App with updated gitIgnore

---
+++ app/src/main/java/auspost/com/au/quake/QuakeMainActivity.java
@@ -0,0 +1,13 @@
+package auspost.com.au.quake;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class QuakeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quake_main);
+    }
+}
